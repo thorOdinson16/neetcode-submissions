952af517abd602ec0cd0f3c5f649ef858d59631b
Abhijna DS <abhijnads16@gmail.com>
2026-07-08 01:55:18 +0530

    Delete Data Structures & Algorithms/is-anagram/submission-1.py

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        x = list(s)
-        y = list(t)
-        if sorted(x) == sorted(y):
-            return True
-        else:
-            return False
